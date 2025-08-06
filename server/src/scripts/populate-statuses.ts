36c99337b5ba0cc59b081a0688d00c5c6be759ab
Timothy <timothyrwashburn@gmail.com>
2025-08-06 15:27:03 -0700

    update status names

--- server/src/scripts/populate-statuses.ts
+++ server/src/scripts/populate-statuses.ts
@@ -8,12 +8,12 @@ export const populateStatusesScript: ScriptConfig = {
         console.log('Starting status population...');
 
         const statusMappings = [
-            { status_id: 1, status_name: 'confirmed (info session)' },
-            { status_id: 106, status_name: 'tentative' },
-            { status_id: 367, status_name: 'info only' },
-            { status_id: 778, status_name: 'confirmed (conference)' },
+            { status_id: 1, status_name: 'Confirmed' },
+            { status_id: 106, status_name: 'Tentative' },
+            { status_id: 367, status_name: 'Info Only' },
+            { status_id: 778, status_name: 'In Planning' },
             { status_id: 781, status_name: 'WEB CONFIRMED' },
-            { status_id: 791, status_name: 'venue setup/teardown' }
+            { status_id: 791, status_name: 'Venue Setup and Teardown' }
         ];
 
         console.log(`Creating ${statusMappings.length} status records`);
